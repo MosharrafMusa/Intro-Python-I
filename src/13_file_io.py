6a9126c33afb5609e196225dbffb69770f07dcbc
Mosharraf Musa <mosharrafny@gmail.com>
2020-08-07 16:02:06 -0400

    Discovered cal.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+print(f.read())
+f.close()
+print(f.closed)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +22,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write(["Hello\n", "This-is\n", "Mosharraf\n"])
+f.close()
+print(f.closed)
+f = open('bar.txt', 'r')
+print(f.read())
+f.close()
+print(f.closed)
--- src/14_cal.py
+++ src/14_cal.py
@@ -2,7 +2,6 @@
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
-
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
@@ -17,11 +16,9 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
-
 Note: the user should provide argument input (in the initial call to run the file) and not 
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
-
 This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
@@ -30,3 +27,17 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+
+user_input = input("Enter Date ").split(" ")
+length = len(user_input)
+now = datetime.now()
+
+if user_input[0] == "":
+    print(calendar.month(now.year, now.month))
+elif length == 1:
+    print(calendar.month(now.year, int(user_input[0])))
+elif length == 2:
+    print(calendar.month(int(user_input[1]), int(user_input[0])))
+else:
+    print("Enter month in M format and a year as YYYY")
